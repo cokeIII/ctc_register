8d7f9980652bd62384c73ac9927bc4944497fa97
cokeIII <coke2484@gmail.com>
2022-02-20 12:47:00 +0700

    f

--- getName.php
+++ getName.php
@@ -1,5 +1,7 @@
 <?php
 require_once "connect.php";
+header('Content-Type: text/html; charset=UTF-8');
+
 session_start();
 $student_id = $_POST["student_id"];
 
@@ -9,7 +11,7 @@ $resShow = mysqli_query($conn, $sqlShow);
 $rowShow = mysqli_fetch_array($resShow);
 $numRow = mysqli_num_rows($resShow);
 if ($numRow > 0) {
-    echo $rowShow["student_id"]." ".$rowShow["prefix"] . $rowShow["fname"] . " " . $rowShow["lname"];
+    echo $rowShow["student_id"] . " " . $rowShow["prefix"] . $rowShow["fname"] . " " . $rowShow["lname"];
 } else {
     echo "รหัสนักเรียนไม่ถูกต้อง";
 }
